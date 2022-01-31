2756147d2cd97e1fa3ee5607537d2ae3bc191377
Michael Dally <mddally@gmail.com>
2022-01-31 17:47:09 -0500

    commit tree dbd8b0e3de2fca5d263b4ece40d4999343fc5bf3

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return pickupLocation >= 42 ? pickupLocation - 42 : 42 - pickupLocation;
+}
+
+function distanceFromHqInFeet(pickupLocation){
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    if(start >= destination){
+        return (start - destination) * 264;
+    } else{
+        return (destination - start) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination);
+    if(distance > 2500){
+        return 'cannot travel that far'
+    } else if(distance > 2000){
+        return 25;
+    } else if(distance > 400){
+        return (distance - 400) * .02;
+    } else {
+        return 0;
+    }
+}
